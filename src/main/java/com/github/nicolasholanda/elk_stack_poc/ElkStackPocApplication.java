909ce9d0d3c6ef689ecfc4db694317869ad0fa1d
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-11-07 21:21:25 -0300

    initial commit

---
+++ src/main/java/com/github/nicolasholanda/elk_stack_poc/ElkStackPocApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.elk_stack_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkStackPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkStackPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nicolasholanda/elk_stack_poc/ElkStackPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.elk_stack_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkStackPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
